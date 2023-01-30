ddef53efb658ae2aa8ad2acedf2bc2727ecf345e
Yogesh Kumar <yogesh.kumar@trilogy.com>
2023-01-30 20:47:38 +0530

    wip

--- gen_installer.sh
+++ gen_installer.sh
@@ -36,9 +36,10 @@ cd cloudfix-linter
 VERSION_TAG=$(git describe --tags --abbrev=0)
 # Install cloudfix-linter-cloudformation
 echo "Installing cloudfix-linter-cloudformation"
-file_name=cloudfix-linter-cloudformation_\${PLATFORM}
-(wget https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/\${VERSION_TAG}/download/\${file_name} -O \${file_name} --no-check-certificate \
-  && mv \${file_name} cloudfix-linter-cloudformation)
+FILE_NAME=cloudfix-linter-cloudformation_\${PLATFORM}
+DOWNLOAD_ADDRESS=https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/\${VERSION_TAG}/download
+(wget \${DOWNLOAD_ADDRESS}/\${FILE_NAME} -O \${FILE_NAME} --no-check-certificate \
+  && mv \${FILE_NAME} cloudfix-linter-cloudformation)
 # Setting alias for cloudfix-linter so that it can be used via command line without referencing the binary path
 path=\$(pwd)
 path+="/cloudfix-linter-cloudformation"
@@ -47,7 +48,7 @@ chmod +x cloudfix-linter-cloudformation
 
 # Python file for CFN-Lint
 echo "Downloading required python files"
-(wget https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/\${VERSION_TAG}/download/mynewrule.py -O mynewrule.py --no-check-certificate )
+(wget \${DOWNLOAD_ADDRESS}/mynewrule.py -O mynewrule.py --no-check-certificate )
 EOF1
 
 cat >install.ps1 <<EOF2
@@ -70,7 +71,8 @@ if (-Not (Get-Item \$OUT_PATH)) { New-Item -Path \$OUT_PATH -ItemType Directory 
 # Install cloudfix-linter
 Write-Output "Installing cloudfix-linter-cloudformation........"
 \$OUT_PATH_CFT=\$OUT_PATH+"cloudfix-linter-cloudformation.exe"
-Invoke-WebRequest -URI https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/\${VERSION_TAG}/download/cloudfix-linter-cloudformation_\${PLATFORM}.exe -OutFile \$OUT_PATH_CFT
+\$DOWNLOAD_ADDRESS="https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/"+\$VERSION_TAG+"/download"
+Invoke-WebRequest -URI \${DOWNLOAD_ADDRESS}/cloudfix-linter-cloudformation_\${PLATFORM}.exe -OutFile \$OUT_PATH_CFT
 \$TEMP=\$OUT_PATH+"cloudfix-linter-cloudformation.exe"
 Set-Alias -Name cloudfix-linter-cloudformation -Value \$TEMP -Scope Global
 Write-Output "Cloudfix-linter installed successfully"
@@ -78,7 +80,7 @@ Write-Output "Cloudfix-linter installed successfully"
 
 Write-Output "Installing cloudfix-linter-cloudformation........"
 \$OUT_PATH_CFT=\$OUT_PATH+"mynewrule.py"
-Invoke-WebRequest -URI https://github.com/trilogy-group/Cloudfix-linter-Cloudformation-Release/releases/tag/\${VERSION_TAG}/download/mynewrule.py -OutFile \$OUT_PATH_CFT
+Invoke-WebRequest -URI \${DOWNLOAD_ADDRESS}/mynewrule.py -OutFile \$OUT_PATH_CFT
 
 
 # Installing CFN-Lint
